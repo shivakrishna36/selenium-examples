4652aa6fb93393b906a2359ee84f837ec634a659
SHIVA KRISHNA <45350264+shivakrishna36@users.noreply.github.com>
2020-04-20 21:19:06 +0530

    TestNG-Jenkins

---
+++ testNG-jenkins/src/test/java/com/selenium/NewTest.java
@@ -0,0 +1,14 @@
+package com.selenium;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+  @Test
+  public void f() {
+	  System.out.println("hello");
+	  assertEquals(true, true);
+  }
+}  
---
+++ testNG-jenkins/src/test/java/com/selenium/secondTest.java
@@ -0,0 +1,14 @@
+package com.selenium;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class secondTest {
+	
+  @Test
+  public void f() {
+	  System.out.println("world");
+	  assertEquals(1,1);
+  }
+}
